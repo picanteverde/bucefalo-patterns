a342f3371724f595de40684bcf6d0ec297846f4d
Gustavo ELias <sicompsrl@hotmail.com>
2012-07-12 13:16:31 -0300

    namespaces added

--- src/bucefalo.namespace.js
+++ src/bucefalo.namespace.js
@@ -1,12 +1,29 @@
 var bucefalo = bucefalo || {};
 bucefalo.patterns = bucefalo.patterns || {};
 if(!bucefalo.namespace){
-	bucefalo.namepsace = function(namepsace, object){
-		var global = (function(){ 
+	bucefalo.namespace = function(namespace, object){
+		var o,
+			ar,
+			len,
+			i,
+			global = (function(){ 
 				return this;
 			}());
-		};
-		
+		o = global;
+		ar = namespace.split(".");
+		len = ar.length;
+		for (i = 0; i < len; i += 1){
+			if (!o.hasOwnProperty(ar[i])) {
+				if (i === len - 1) { 
+					o[ar[i]] = object;
+				}
+				else {
+					o[ar[i]] = {};
+				}
+			}
+			o = o[ar[i]];
+
+		}
 	};
 
 }
