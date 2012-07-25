734c3ca0a95aa925246886f6fe8caf9a6d889219
Diego Ferreyra <diegof18@gmail.com>
2012-07-25 14:35:53 -0300

    ref #15, #17, #19, #21
    add jquey to mocha and corrections for passing tests.

--- test/bucefalo-patterns.persona.js
+++ test/bucefalo-patterns.persona.js
@@ -1,6 +1,7 @@
  describe("bucefalo.pattenrs.persona",function(){
 
-it("should create a new persona and test hablar",function(){
-	var a = new Persona();
-	expect(a.hablar()).to.equal("hola");
+	it("should create a new persona and test hablar",function(){
+		var a = new Persona();
+		expect(a.hablar()).to.equal("hola");
+	});
 });
--- test/bucefalo-patterns.personaPower.js
+++ test/bucefalo-patterns.personaPower.js
@@ -1,6 +1,7 @@
 describe("bucefalo.pattenrs.personaPower",function(){
 
-it("should create a new persona and test hablar",function(){
-	var a = crearPersona();
-	expect(a.hablar()).to.equal("hola");
+	it("should create a new persona and test hablar",function(){
+		var a = createPersona();
+		expect(a.hablar()).to.equal("hola");
+	});
 });
